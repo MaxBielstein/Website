dc9ebf4700c45d9a67c28e959c8f3d43623dae1b
Max Bielstein <mbielstein2@huskers.unl.edu>
2023-02-19 16:06:50 -0600

    Change animateDiv function
    Changes the way the divs move in the home screen

--- max-website/src/Components/Background.tsx
+++ max-website/src/Components/Background.tsx
@@ -6,16 +6,44 @@ import { getHomeTextItems } from "../Utilities/FinalData";
 
 function AnimateDiv(name: string, widthTranslate: string, heightTranslate: string) {
 
+    const top = 'translate(' + 0 + ', ' + (-50) + 'vh)'
+    const bottom = 'translate(' + 0 + ', ' + 50 + 'vh)'
+    const right = 'translate(' + 50 + 'vw, ' + 0 + ')'
+    const left = 'translate(' + (-50) + 'vw, ' + 0 + ')'
+    
+    
+    
     console.log(widthTranslate); 
     if (widthTranslate !== "0" && heightTranslate !== "0"){
         for (let i = 0; i < getHomeTextItems().length; i++){
-            console.log('ran')
             const animatedDiv = document.getElementById(getHomeTextItems()[i]);
+            // Moving all buttons that are not the one clicked off of the screen
             if (animatedDiv && name !== animatedDiv.id) {
-                
+                if (animatedDiv.className.includes('top')){
+                    animatedDiv.style.transform = top;
+                } else if (animatedDiv.className.includes('bottom')){
+                    console.log('ran');
+                    animatedDiv.style.transform = bottom;
+                } else if (animatedDiv.className.includes('right')){
+                    animatedDiv.style.transform = right;
+                } else if (animatedDiv.className.includes('left')){
+                    animatedDiv.style.transform = left;
+                }
 
-                
+                // Moving the button clicked to correct position
+            } else if (animatedDiv){
+                if (animatedDiv.className === 'top'){
+                    animatedDiv.style.transform = top;
+                } else if (animatedDiv.className === 'bottom'){
+                    animatedDiv.style.transform = bottom;
+                } else if (animatedDiv.className === 'right'){
+                    animatedDiv.style.transform = right;
+                } else if (animatedDiv.className === 'left'){
+                    animatedDiv.style.transform = 'translate(' + widthTranslate + '%, ' + heightTranslate + ')';
+                }
             }
+
+            
         }
     }
 
@@ -28,7 +56,7 @@ function AnimateDiv(name: string, widthTranslate: string, heightTranslate: strin
 
 
 export function AboutMeButton() {
-    return (<div className="about-me" id="about-me" onClick={() => {
+    return (<div className="about-me left" id="about-me" onClick={() => {
         AnimateDiv('about-me', '20vw', '-25vh');
     }}>
         About Me
@@ -37,7 +65,7 @@ export function AboutMeButton() {
 
 export function ProjectsButton() {
     return (
-        <div className="projects" id="projects" onClick={() => {
+        <div className="projects bottom" id="projects" onClick={() => {
             AnimateDiv('projects', '-8vw', '-48vh');
             AnimateDiv('about-me', '-200px', '310px');
         }}>
@@ -48,7 +76,7 @@ export function ProjectsButton() {
 
 export function ResumeButton() {
     return (
-        <div className="resume" id="resume" onClick={() => {
+        <div className="resume right" id="resume" onClick={() => {
             AnimateDiv('resume', '5vw', '5vh');
         }} >
             Resume
@@ -61,7 +89,7 @@ export function ResumeButton() {
 export function Background() {
     return (
         <div className="background">
-            <div className="title" id = "title">
+            <div className="title top" id = "title">
                 Max Bielstein
             </div>
             <AboutMeButton />
