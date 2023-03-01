179da4541725b9b58e4f5e7056cdc1d1f9ec1d7c
Max Bielstein <mbielstein2@huskers.unl.edu>
2023-03-01 02:37:07 -0600

    Added props interface for Background file

--- max-website/src/Components/Background.tsx
+++ max-website/src/Components/Background.tsx
@@ -3,78 +3,85 @@ import "../index.scss";
 import "./Background.scss";
 import { getHomeTextItems } from "../Utilities/FinalData";
 import { centerElementById } from "../Utilities/MovingDivs";
+import { useState } from 'react';
 
-
-function AnimateDiv(name: string) {
-
-    const top = 'translate(' + 0 + ', ' + (-50) + 'vh)'
-    const bottom = 'translate(' + 0 + ', ' + 50 + 'vh)'
-    const right = 'translate(' + 50 + 'vw, ' + 0 + ')'
-    const left = 'translate(' + (-50) + 'vw, ' + 0 + ')'
-    
-        for (let i = 0; i < getHomeTextItems().length; i++){
-            // Get Element Clicked
-            const animatedDiv = document.getElementById(getHomeTextItems()[i]);
-            // Moving all buttons that are not the one clicked off of the screen
-            if (animatedDiv && name !== animatedDiv.id) {
-                if (animatedDiv.className.includes('top')){
-                    animatedDiv.style.transform = top;
-                } else if (animatedDiv.className.includes('bottom')){
-                    animatedDiv.style.transform = bottom;
-                } else if (animatedDiv.className.includes('right')){
-                    animatedDiv.style.transform = right;
-                } else if (animatedDiv.className.includes('left')){
-                    animatedDiv.style.transform = left;
-                }
-            } else {
-                // Moving the button clicked to correct position
-                centerElementById(getHomeTextItems()[i]);
-            }
-        }
+interface Props {
+  setActiveButton: (buttonName: string) => void;
+  activeButton: string;
 }
 
+function AnimateDiv(name: string, setActiveButton: (buttonName: string) => void) {
+  const top = 'translate(' + 0 + ', ' + (-50) + 'vh)';
+  const bottom = 'translate(' + 0 + ', ' + 50 + 'vh)';
+  const right = 'translate(' + 50 + 'vw, ' + 0 + ')';
+  const left = 'translate(' + (-50) + 'vw, ' + 0 + ')';
 
-export function AboutMeButton() {
-    return (<div className="about-me left" id="about-me" onClick={() => {
-        AnimateDiv('about-me');
+  for (let i = 0; i < getHomeTextItems().length; i++) {
+    // Get Element Clicked
+    const animatedDiv = document.getElementById(getHomeTextItems()[i]);
+    // Moving all buttons that are not the one clicked off of the screen
+    if (animatedDiv && name !== animatedDiv.id) {
+      if (animatedDiv.className.includes('top')) {
+        animatedDiv.style.transform = top;
+      } else if (animatedDiv.className.includes('bottom')) {
+        animatedDiv.style.transform = bottom;
+      } else if (animatedDiv.className.includes('right')) {
+        animatedDiv.style.transform = right;
+      } else if (animatedDiv.className.includes('left')) {
+        animatedDiv.style.transform = left;
+      }
+    } else {
+      // Moving the button clicked to correct position
+      centerElementById(getHomeTextItems()[i]);
+      setActiveButton(getHomeTextItems()[i]);
+    }
+  }
+}
+
+export function AboutMeButton(props: Props) {
+  return (
+    <div className={`about-me left ${props.activeButton === 'about-me' ? 'active' : ''}`} id="about-me" onClick={() => {
+      AnimateDiv('about-me', props.setActiveButton);
     }}>
-        About Me
-    </div>)
+      About Me
+    </div>
+  );
 }
 
-export function ProjectsButton() {
-    return (
-        <div className="projects bottom" id="projects" onClick={() => {
-            AnimateDiv('projects');
-        }}>
-            Projects
-        </div>
-    );
+export function ProjectsButton(props: Props) {
+  return (
+    <div className={`projects bottom ${props.activeButton === 'projects' ? 'active' : ''}`} id="projects" onClick={() => {
+      AnimateDiv('projects', props.setActiveButton);
+    }}>
+      Projects
+    </div>
+  );
 }
 
-export function ResumeButton() {
-    return (
-        <div className="resume right" id="resume" onClick={() => {
-            AnimateDiv('resume');
-        }} >
-            Resume
-        </div>
-    );
+export function ResumeButton(props: Props) {
+  return (
+    <div className={`resume right ${props.activeButton === 'resume' ? 'active' : ''}`} id="resume" onClick={() => {
+      AnimateDiv('resume', props.setActiveButton);
+    }}>
+      Resume
+    </div>
+  );
 }
-
-
 
 export function Background() {
-    return (
-        <div className="background">
-            <div className="title top" id = "title">
-                Max Bielstein
-            </div>
-            <AboutMeButton />
-            <ProjectsButton />
-            <ResumeButton />
-        </div>
-    );
+  const [activeButton, setActiveButton] = useState('');
+  const props: Props = {
+    setActiveButton: setActiveButton,
+    activeButton: activeButton,
+  };
+  return (
+    <div className="background">
+      <div className={`title top ${activeButton === 'title' ? 'active' : ''}`} id="title">
+        Max Bielstein
+      </div>
+      <AboutMeButton {...props} />
+      <ProjectsButton {...props} />
+      <ResumeButton {...props} />
+    </div>
+  );
 }
-
-
