ad803a8aafe880a3908ac77cfc8a72f569818b19
Max Bielstein <mbielstein2@huskers.unl.edu>
2023-03-01 21:00:44 -0600

    Add code to reset div when clicked twice

--- max-website/src/Components/Background.tsx
+++ max-website/src/Components/Background.tsx
@@ -6,82 +6,100 @@ import { centerElementById } from "../Utilities/MovingDivs";
 import { useState } from 'react';
 
 interface Props {
-  setActiveButton: (buttonName: string) => void;
-  activeButton: string;
+    setActiveButton: (buttonName: string) => void;
+    setLastTranslateInverted: (translate: string) => void,
+    activeButton: string;
+    lastTranslateInverted: string;
 }
 
-function AnimateDiv(name: string, setActiveButton: (buttonName: string) => void) {
-  const top = 'translate(' + 0 + ', ' + (-50) + 'vh)';
-  const bottom = 'translate(' + 0 + ', ' + 50 + 'vh)';
-  const right = 'translate(' + 50 + 'vw, ' + 0 + ')';
-  const left = 'translate(' + (-50) + 'vw, ' + 0 + ')';
+function AnimateDiv(name: string, props: Props) {
 
-  for (let i = 0; i < getHomeTextItems().length; i++) {
-    // Get Element Clicked
-    const animatedDiv = document.getElementById(getHomeTextItems()[i]);
-    // Moving all buttons that are not the one clicked off of the screen
-    if (animatedDiv && name !== animatedDiv.id) {
-      if (animatedDiv.className.includes('top')) {
-        animatedDiv.style.transform = top;
-      } else if (animatedDiv.className.includes('bottom')) {
-        animatedDiv.style.transform = bottom;
-      } else if (animatedDiv.className.includes('right')) {
-        animatedDiv.style.transform = right;
-      } else if (animatedDiv.className.includes('left')) {
-        animatedDiv.style.transform = left;
-      }
-    } else {
-      // Moving the button clicked to correct position
-      centerElementById(getHomeTextItems()[i]);
-      setActiveButton(getHomeTextItems()[i]);
+    if (props.activeButton === name){
+        const element = document.querySelector(`#${name}`) as HTMLElement;
+        element.style.transform  = '';
+        element.style.transform = props.lastTranslateInverted;
+        console.log(props.lastTranslateInverted)
+        return;
     }
-  }
+
+    const top = 'translate(' + 0 + ', ' + (-50) + 'vh)';
+    const bottom = 'translate(' + 0 + ', ' + 50 + 'vh)';
+    const right = 'translate(' + 50 + 'vw, ' + 0 + ')';
+    const left = 'translate(' + (-50) + 'vw, ' + 0 + ')';
+    let buttonClicked = '';
+
+    for (let i = 0; i < getHomeTextItems().length; i++) {
+        // Get Element Clicked
+        const animatedDiv = document.getElementById(getHomeTextItems()[i]);
+        // Moving all buttons that are not the one clicked off of the screen
+        if (animatedDiv && name !== animatedDiv.id) {
+            if (animatedDiv.className.includes('top')) {
+                animatedDiv.style.transform = top;
+            } else if (animatedDiv.className.includes('bottom')) {
+                animatedDiv.style.transform = bottom;
+            } else if (animatedDiv.className.includes('right')) {
+                console.log(';asdokawpdokasd')
+                animatedDiv.style.transform = right;
+            } else if (animatedDiv.className.includes('left')) {
+                animatedDiv.style.transform = left;
+            }
+        } else {
+            // Moving the button clicked to correct position
+            props.setLastTranslateInverted(centerElementById(getHomeTextItems()[i]));
+            buttonClicked = getHomeTextItems()[i];
+        }
+    }
+    props.setActiveButton(buttonClicked);
+
 }
 
 export function AboutMeButton(props: Props) {
-  return (
-    <div className={`about-me left ${props.activeButton === 'about-me' ? 'active' : ''}`} id="about-me" onClick={() => {
-      AnimateDiv('about-me', props.setActiveButton);
-    }}>
-      About Me
-    </div>
-  );
+    return (
+        <div className={`about-me left ${props.activeButton === 'about-me' ? 'active' : ''}`} id="about-me" onClick={() => {
+            AnimateDiv('about-me', props);
+        }}>
+            About Me
+        </div>
+    );
 }
 
 export function ProjectsButton(props: Props) {
-  return (
-    <div className={`projects bottom ${props.activeButton === 'projects' ? 'active' : ''}`} id="projects" onClick={() => {
-      AnimateDiv('projects', props.setActiveButton);
-    }}>
-      Projects
-    </div>
-  );
+    return (
+        <div className={`projects bottom ${props.activeButton === 'projects' ? 'active' : ''}`} id="projects" onClick={() => {
+            AnimateDiv('projects', props);
+        }}>
+            Projects
+        </div>
+    );
 }
 
 export function ResumeButton(props: Props) {
-  return (
-    <div className={`resume right ${props.activeButton === 'resume' ? 'active' : ''}`} id="resume" onClick={() => {
-      AnimateDiv('resume', props.setActiveButton);
-    }}>
-      Resume
-    </div>
-  );
+    return (
+        <div className={`resume right ${props.activeButton === 'resume' ? 'active' : ''}`} id="resume" onClick={() => {
+            AnimateDiv('resume', props);
+        }}>
+            Resume
+        </div>
+    );
 }
 
 export function Background() {
-  const [activeButton, setActiveButton] = useState('');
-  const props: Props = {
-    setActiveButton: setActiveButton,
-    activeButton: activeButton,
-  };
-  return (
-    <div className="background">
-      <div className={`title top ${activeButton === 'title' ? 'active' : ''}`} id="title">
-        Max Bielstein
-      </div>
-      <AboutMeButton {...props} />
-      <ProjectsButton {...props} />
-      <ResumeButton {...props} />
-    </div>
-  );
+    const [activeButton, setActiveButton] = useState('');
+    const [lastTranslateInverted, setLastTranslateInverted] = useState('');
+    let props: Props = {
+        setLastTranslateInverted: setLastTranslateInverted,
+        setActiveButton: setActiveButton,
+        activeButton: activeButton,
+        lastTranslateInverted: lastTranslateInverted,
+    };
+    return (
+        <div className="background">
+            <div className={`title top ${activeButton === 'title' ? 'active' : ''}`} id="title">
+                Max Bielstein
+            </div>
+            <AboutMeButton {...props} />
+            <ProjectsButton {...props} />
+            <ResumeButton {...props} />
+        </div>
+    );
 }
--- max-website/src/Utilities/MovingDivs.tsx
+++ max-website/src/Utilities/MovingDivs.tsx
@@ -1,4 +1,4 @@
-
+import { Decimal } from 'decimal.js';
 // Centers an HTMLElement in the top middle of the screen
 export function centerElementById(id: string) {
     const element = document.querySelector(`#${id}`) as HTMLElement;
@@ -13,4 +13,18 @@ export function centerElementById(id: string) {
     const x = (windowWidth - elementWidth) / 2 - element.offsetLeft;
     const y = (windowHeight * 0.15) - element.offsetTop;
     element.style.transform = `translate(${x}px, ${y}px)`;
+
+    return invertTranslateString(`translate(${x}px, ${y}px)`);
 }
+
+function invertTranslateString(translateString: string): string {
+    const values = translateString.match(/-?\d+(\.\d+)?/g)?.map(value => new Decimal(value));
+    const xUnit = /-?\d+(\.\d+)?(px|%)/g.exec(translateString)?.[2] || '';
+    const yUnit = /-?\d+(\.\d+)?(px|%)/g.exec(translateString)?.[2] || '';
+    if (values === undefined){
+        return ''
+    }
+    const x = new Decimal(-values[0]) + (xUnit === 'px' ? 'px' : '');
+    const y = new Decimal(-values[1]) + (yUnit === 'px' ? 'px' : '');
+    return `translate(${x.toString()}${xUnit}, ${y.toString()}${yUnit})`;
+  }
