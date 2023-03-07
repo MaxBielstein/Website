2e4a83291b6b85363408ef3d8b950094b901a440
Max Bielstein <mbielstein2@huskers.unl.edu>
2023-03-07 04:57:18 -0600

    Fix linting error

--- max-website/setupProxy.js
+++ max-website/setupProxy.js
@@ -1,11 +1,11 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
-export default function(app) {
-  app.use('/api', createProxyMiddleware({
-    target: 'http://localhost:8080',
-    changeOrigin: true,
-    pathRewrite: {
-      '^/api': '/'
-    }
-  }));
-};
+export default function proxyMiddleware(app) {
+    app.use('/api', createProxyMiddleware({
+      target: 'http://localhost:8080',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api': '/'
+      }
+    }));
+  };
