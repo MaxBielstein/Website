1ac97e51de2ec024a1f312326a351c177f384906
Max Bielstein <mbielstein2@huskers.unl.edu>
2025-08-18 22:31:25 -0500

    Inject css during load

--- max-website/src/App.tsx
+++ max-website/src/App.tsx
@@ -1,5 +1,7 @@
+// App.tsx
 import * as React from 'react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
+import { StyledEngineProvider } from '@mui/material/styles';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { theme } from './theme';
 import Layout from './layout/Layout';
@@ -7,20 +9,21 @@ import AboutPage from './pages/AboutPage';
 import ProjectTrackerPage from './pages/ProjectTrackerPage';
 import PortfolioPage from './pages/PortfolioPage';
 
-
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<AboutPage />} />
-            <Route path="/project-tracker" element={<ProjectTrackerPage />} />
-            <Route path="/portfolio" element={<PortfolioPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<AboutPage />} />
+              <Route path="/project-tracker" element={<ProjectTrackerPage />} />
+              <Route path="/portfolio" element={<PortfolioPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
 }
